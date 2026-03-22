2740acae9442f0901191a91378afc852e19430df
kanazawa <andy2leeqaz@hotmail.com>
2026-03-22 18:17:06 +0800

    Add files via upload

--- CudaLib/NDCuArray.java
+++ CudaLib/NDCuArray.java
@@ -28,10 +28,28 @@ public class NDCuArray implements AutoCloseable {
                 na_arr_mem_seg.setAtIndex(JAVA_DOUBLE, i, _arr[i]);
             }
             int na_arr_b_size = (int)na_arr_mem_seg.byteSize();
-            CudaMemObj _cu_na_arr = CudaNumLib.cudaMalloc(na_arr_b_size);
-            CudaNumLib.cudaMemcpy(_cu_na_arr, na_arr_mem_seg, na_arr_b_size, cudaMemcpyHostToDevice);
+            CudaMemObj _cu_na_arr = cudaMalloc(na_arr_b_size);
+            cudaMemcpy(_cu_na_arr, na_arr_mem_seg, na_arr_b_size, cudaMemcpyHostToDevice);
             cu_na_arr = _cu_na_arr;
         }
+    }
+
+    public NDCuArray(int... _shape) {
+        arr_size = 1;
+        for (int i = 0; i < _shape.length; i++) {
+            arr_size*=_shape[i];
+        }
+        shape = _shape.clone();
+
+        // calculate stride array settings. depth - row - col (C/Cpp, numpy like)
+        int[] _stride = new int[shape.length];
+        int accu_val = 1;
+        for (int i = (_stride.length-1); i >= 0; i--) {
+            _stride[i] = accu_val;
+            accu_val*=shape[i];
+        }
+        stride = _stride;
+        cu_na_arr = cudaMalloc(arr_size*CU_DOUBLE);
     }
 
     public NDCuArray(CudaMemObj _cu_na_arr, int _arr_size, int[] _shape, int[] _stride) {
@@ -44,6 +62,19 @@ public class NDCuArray implements AutoCloseable {
     @Override
     public void close() {
         cudaFree(cu_na_arr);
+    }
+
+    public void print() {
+        try (Arena tmp_arena = Arena.ofConfined()) {
+            MemorySegment na_arr_mem_seg = tmp_arena.allocate(JAVA_DOUBLE, arr_size); // get only 1 element
+            int na_arr_size = (int)na_arr_mem_seg.byteSize();
+            cudaMemcpy(na_arr_mem_seg, cu_na_arr, na_arr_size, cudaMemcpyDeviceToHost);
+            System.out.printf("[ ");
+            for (int i = 0; i < arr_size; i++) {
+                System.out.printf("%f ", na_arr_mem_seg.getAtIndex(JAVA_DOUBLE, i));
+            }
+            System.out.printf("]\n");
+        }
     }
 
     //a0 a1 a2    a3       ...
@@ -84,65 +115,153 @@ public class NDCuArray implements AutoCloseable {
         return 0;
     }
 
-    public NDCuArray get(NDCuSlice... ndcu_slice) {
+    public void set(NDCuArray b_arr, NDCuSlice... slices) {
         int[] re_shape = new int[shape.length];
-        int[] re_stride = new int[shape.length];
+        int re_elem = 1;
+        
+        if (slices.length != shape.length) {
+            IO.println("Error get size.");
+            return;
+        }
+        
+        for (int i = 0; i < slices.length; i++) {
+            if (slices[i].start > slices[i].end) {
+                IO.println("Error access array index when start is larget than end index.");
+                return;
+            }
+            if ((slices[i].start < 0) || (slices[i].start > shape[i])) {
+                IO.println("Error access array index.");
+                return;
+            }
+            if ((slices[i].end < 0) || (slices[i].end > shape[i])) {
+                IO.println("Error access array index.");
+                return;
+            }
+        }
 
-        if (ndcu_slice.length != shape.length) {
+        try (Arena tmp_arena = Arena.ofConfined()) {
+            MemorySegment stride_mem_seg = tmp_arena.allocate(JAVA_INT, shape.length);
+            MemorySegment re_stride_mem_seg = tmp_arena.allocate(JAVA_INT, shape.length);
+            MemorySegment slice_start_mem_seg = tmp_arena.allocate(JAVA_INT, shape.length);
+            
+            // set each slice len and their slice start index
+            int dim_len = 0;
+            for (int i = 0; i < shape.length; i++) {
+                dim_len = slices[i].end - slices[i].start;
+                re_elem*=dim_len;
+                re_shape[i] = dim_len;
+                slice_start_mem_seg.setAtIndex(JAVA_INT, i, slices[i].start);
+            }
+
+            // set re_stride mem and original stride mem
+            int accu_val = 1;
+            for (int i = (shape.length-1); i >= 0; i--) {
+                re_stride_mem_seg.setAtIndex(JAVA_INT, i, accu_val);
+                accu_val*=re_shape[i];
+            }
+
+            for (int i = 0; i < stride.length; i++) {
+                stride_mem_seg.setAtIndex(JAVA_INT, i, stride[i]);
+            }
+
+            // GPU allocate mem
+            CudaMemObj cu_nd_slice_start = cudaMalloc((int)slice_start_mem_seg.byteSize());
+            cudaMemcpy(cu_nd_slice_start, slice_start_mem_seg, (int)slice_start_mem_seg.byteSize(), cudaMemcpyHostToDevice);
+            CudaMemObj cu_re_stride = cudaMalloc((int)re_stride_mem_seg.byteSize());
+            cudaMemcpy(cu_re_stride, re_stride_mem_seg, (int)re_stride_mem_seg.byteSize(), cudaMemcpyHostToDevice);
+            CudaMemObj cu_stride = cudaMalloc((int)stride_mem_seg.byteSize());
+            cudaMemcpy(cu_stride, stride_mem_seg, (int)stride_mem_seg.byteSize(), cudaMemcpyHostToDevice);
+
+            cuda_set_slice(
+                threadsPerBlock,
+                re_elem, 
+                shape.length, 
+                cu_nd_slice_start,
+                cu_re_stride,
+                cu_stride,
+                cu_na_arr,
+                b_arr.cu_na_arr
+            );
+            cudaDeviceSynchronize();
+        }
+    }
+
+    public NDCuArray get(NDCuSlice... slices) {
+        int[] re_shape = new int[shape.length];
+        int[] re_stride;
+        int re_elem = 1;
+        CudaMemObj _cu_na_arr;
+        
+        if (slices.length != shape.length) {
             IO.println("Error get size.");
             return null;
         }
-        int re_elem = 1, dim_len = 0;
-        for (int i = 0; i < shape.length; i++) {
-            dim_len = ndcu_slice[i].end - ndcu_slice[i].start;
-            re_elem*=dim_len;
-            re_shape[i] = dim_len;
+        
+        for (int i = 0; i < slices.length; i++) {
+            if (slices[i].start > slices[i].end) {
+                IO.println("Error access array index when start is larget than end index.");
+                return null;
+            }
+            if ((slices[i].start < 0) || (slices[i].start > shape[i])) {
+                IO.println("Error access array index.");
+                return null;
+            }
+            if ((slices[i].end < 0) || (slices[i].end > shape[i])) {
+                IO.println("Error access array index.");
+                return null;
+            }
         }
 
-        int accu_val = 1;
-        for (int i = (re_stride.length-1); i >= 0; i--) {
-            re_stride[i] = accu_val;
-            accu_val*=re_shape[i];
-        }
-
-        double[] re_res = new double[re_elem];
-        for (int i = 0; i < re_elem; i++) {
-            int i_rem = i;
-            int[] query_shapes = new int[shape.length];
-            for (int j = 0; j < query_shapes.length; j++) {
-                query_shapes[j] = ndcu_slice[j].start;
-            }
-
-            for (int j = 0; j < re_stride.length; j++) {
-                int i_div = i_rem / re_stride[j];
-                i_rem = i_rem % re_stride[j];
-                query_shapes[j]+=i_div;
-            }
-
-            int index = 0;
-            for (int k = 0; k < stride.length; k++) {
-                index+=query_shapes[k] * stride[k];
-            }
-
-            MemorySegment na_arr_mem_seg = arena.allocate(JAVA_DOUBLE, 1); // get only 1 element
-            int na_arr_b_size = (int)na_arr_mem_seg.byteSize();
-            cudaMemcpy(na_arr_mem_seg, cu_na_arr, na_arr_b_size, 8*index, cudaMemcpyDeviceToHost);
-            double res = na_arr_mem_seg.getAtIndex(JAVA_DOUBLE, 0);
-            IO.println(res);
-            re_res[i] = res;
-        }
-
-        // GPU copy
-        CudaMemObj _cu_na_arr;
         try (Arena tmp_arena = Arena.ofConfined()) {
-            MemorySegment na_arr_mem_seg = tmp_arena.allocate(JAVA_DOUBLE, re_elem);
-            for (int i = 0; i < re_elem; i++) {
-                na_arr_mem_seg.setAtIndex(JAVA_DOUBLE, i, re_res[i]);
+            MemorySegment stride_mem_seg = tmp_arena.allocate(JAVA_INT, shape.length);
+            MemorySegment re_stride_mem_seg = tmp_arena.allocate(JAVA_INT, shape.length);
+            MemorySegment slice_start_mem_seg = tmp_arena.allocate(JAVA_INT, shape.length);
+            
+            // set each slice len and their slice start index
+            int dim_len = 0;
+            for (int i = 0; i < shape.length; i++) {
+                dim_len = slices[i].end - slices[i].start;
+                re_elem*=dim_len;
+                re_shape[i] = dim_len;
+                slice_start_mem_seg.setAtIndex(JAVA_INT, i, slices[i].start);
             }
-            int na_arr_b_size = (int)na_arr_mem_seg.byteSize();
-            _cu_na_arr = CudaNumLib.cudaMalloc(na_arr_b_size);
-            CudaNumLib.cudaMemcpy(_cu_na_arr, na_arr_mem_seg, na_arr_b_size, cudaMemcpyHostToDevice);
+
+            // set re_stride mem and original stride mem
+            int accu_val = 1;
+            for (int i = (shape.length-1); i >= 0; i--) {
+                re_stride_mem_seg.setAtIndex(JAVA_INT, i, accu_val);
+                accu_val*=re_shape[i];
+            }
+
+            for (int i = 0; i < stride.length; i++) {
+                stride_mem_seg.setAtIndex(JAVA_INT, i, stride[i]);
+            }
+
+            // GPU allocate mem
+            CudaMemObj res_cu_na_arr = cudaMalloc(re_elem * CU_DOUBLE);
+            CudaMemObj cu_nd_slice_start = cudaMalloc((int)slice_start_mem_seg.byteSize());
+            cudaMemcpy(cu_nd_slice_start, slice_start_mem_seg, (int)slice_start_mem_seg.byteSize(), cudaMemcpyHostToDevice);
+            CudaMemObj cu_re_stride = cudaMalloc((int)re_stride_mem_seg.byteSize());
+            cudaMemcpy(cu_re_stride, re_stride_mem_seg, (int)re_stride_mem_seg.byteSize(), cudaMemcpyHostToDevice);
+            CudaMemObj cu_stride = cudaMalloc((int)stride_mem_seg.byteSize());
+            cudaMemcpy(cu_stride, stride_mem_seg, (int)stride_mem_seg.byteSize(), cudaMemcpyHostToDevice);
+
+            cuda_get_slice(
+                threadsPerBlock,
+                re_elem, 
+                shape.length, 
+                cu_nd_slice_start,
+                cu_re_stride,
+                cu_stride,
+                res_cu_na_arr,
+                cu_na_arr
+            );
+            cudaDeviceSynchronize();
+            
+            re_stride = re_stride_mem_seg.toArray(JAVA_INT);
+            _cu_na_arr = res_cu_na_arr;
         }
+        
         return new NDCuArray(_cu_na_arr, re_elem, re_shape, re_stride);
     }
 
@@ -160,13 +279,13 @@ public class NDCuArray implements AutoCloseable {
 
         MemorySegment na_arr_mem_seg = arena.allocate(JAVA_DOUBLE, 1); // get only 1 element
         int na_arr_b_size = (int)na_arr_mem_seg.byteSize();
-        cudaMemcpy(na_arr_mem_seg, cu_na_arr, na_arr_b_size, 8*cal_idx_loc(indexes), cudaMemcpyDeviceToHost);
+        cudaMemcpy(na_arr_mem_seg, cu_na_arr, na_arr_b_size, CU_DOUBLE*cal_idx_loc(indexes), cudaMemcpyDeviceToHost);
         double res = na_arr_mem_seg.getAtIndex(JAVA_DOUBLE, 0);
 
         return res;
     }
 
-    public void set(double value, int... indexes) {
+    public void set(double num, int... indexes) {
         if (indexes.length != shape.length) {
             IO.println("Error get size.");
             return;
@@ -179,9 +298,9 @@ public class NDCuArray implements AutoCloseable {
         }
 
         MemorySegment na_arr_mem_seg = arena.allocate(JAVA_DOUBLE, 1); // set only 1 element
-        na_arr_mem_seg.setAtIndex(JAVA_DOUBLE, 0, value);
+        na_arr_mem_seg.setAtIndex(JAVA_DOUBLE, 0, num);
         int na_arr_b_size = (int)na_arr_mem_seg.byteSize();
-        cudaMemcpy(cu_na_arr, na_arr_mem_seg, na_arr_b_size, 8*cal_idx_loc(indexes), cudaMemcpyHostToDevice);
+        cudaMemcpy(cu_na_arr, na_arr_mem_seg, na_arr_b_size, CU_DOUBLE*cal_idx_loc(indexes), cudaMemcpyHostToDevice);
     }
 
     // Ops
@@ -189,8 +308,9 @@ public class NDCuArray implements AutoCloseable {
         if (arr_size != b_arr.arr_size)
             return null;
 
-        CudaMemObj _cu_na_arr = cudaMalloc(arr_size * 8);
-        cuda_add(256, arr_size, cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        CudaMemObj _cu_na_arr = cudaMalloc(arr_size * CU_DOUBLE);
+        cuda_add(threadsPerBlock, arr_size, cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        cudaDeviceSynchronize();
         return new NDCuArray(_cu_na_arr, arr_size, shape, stride);
     }
 
@@ -198,8 +318,9 @@ public class NDCuArray implements AutoCloseable {
         if (a_arr.arr_size != b_arr.arr_size)
             return null;
 
-        CudaMemObj _cu_na_arr = cudaMalloc(a_arr.arr_size * 8);
-        cuda_add(256, a_arr.arr_size, a_arr.cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        CudaMemObj _cu_na_arr = cudaMalloc(a_arr.arr_size * CU_DOUBLE);
+        cuda_add(threadsPerBlock, a_arr.arr_size, a_arr.cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        cudaDeviceSynchronize();
         return new NDCuArray(_cu_na_arr, a_arr.arr_size, a_arr.shape, a_arr.stride);
     }
 
@@ -207,8 +328,9 @@ public class NDCuArray implements AutoCloseable {
         if (arr_size != b_arr.arr_size)
             return null;
 
-        CudaMemObj _cu_na_arr = cudaMalloc(arr_size * 8);
-        cuda_sub(256, arr_size, cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        CudaMemObj _cu_na_arr = cudaMalloc(arr_size * CU_DOUBLE);
+        cuda_sub(threadsPerBlock, arr_size, cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        cudaDeviceSynchronize();
         return new NDCuArray(_cu_na_arr, arr_size, shape, stride);
     }
 
@@ -216,8 +338,9 @@ public class NDCuArray implements AutoCloseable {
         if (a_arr.arr_size != b_arr.arr_size)
             return null;
 
-        CudaMemObj _cu_na_arr = cudaMalloc(a_arr.arr_size * 8);
-        cuda_sub(256, a_arr.arr_size, a_arr.cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        CudaMemObj _cu_na_arr = cudaMalloc(a_arr.arr_size * CU_DOUBLE);
+        cuda_sub(threadsPerBlock, a_arr.arr_size, a_arr.cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        cudaDeviceSynchronize();
         return new NDCuArray(_cu_na_arr, a_arr.arr_size, a_arr.shape, a_arr.stride);
     }
 
@@ -225,8 +348,9 @@ public class NDCuArray implements AutoCloseable {
         if (arr_size != b_arr.arr_size)
             return null;
 
-        CudaMemObj _cu_na_arr = cudaMalloc(arr_size * 8);
-        cuda_mul(256, arr_size, cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        CudaMemObj _cu_na_arr = cudaMalloc(arr_size * CU_DOUBLE);
+        cuda_mul(threadsPerBlock, arr_size, cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        cudaDeviceSynchronize();
         return new NDCuArray(_cu_na_arr, arr_size, shape, stride);
     }
 
@@ -234,8 +358,9 @@ public class NDCuArray implements AutoCloseable {
         if (a_arr.arr_size != b_arr.arr_size)
             return null;
 
-        CudaMemObj _cu_na_arr = cudaMalloc(a_arr.arr_size * 8);
-        cuda_mul(256, a_arr.arr_size, a_arr.cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        CudaMemObj _cu_na_arr = cudaMalloc(a_arr.arr_size * CU_DOUBLE);
+        cuda_mul(threadsPerBlock, a_arr.arr_size, a_arr.cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        cudaDeviceSynchronize();
         return new NDCuArray(_cu_na_arr, a_arr.arr_size, a_arr.shape, a_arr.stride);
     }
 
@@ -243,8 +368,9 @@ public class NDCuArray implements AutoCloseable {
         if (arr_size != b_arr.arr_size)
             return null;
 
-        CudaMemObj _cu_na_arr = cudaMalloc(arr_size * 8);
-        cuda_div(256, arr_size, cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        CudaMemObj _cu_na_arr = cudaMalloc(arr_size * CU_DOUBLE);
+        cuda_div(threadsPerBlock, arr_size, cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        cudaDeviceSynchronize();
         return new NDCuArray(_cu_na_arr, arr_size, shape, stride);
     }
 
@@ -252,12 +378,13 @@ public class NDCuArray implements AutoCloseable {
         if (a_arr.arr_size != b_arr.arr_size)
             return null;
 
-        CudaMemObj _cu_na_arr = cudaMalloc(a_arr.arr_size * 8);
-        cuda_div(256, a_arr.arr_size, a_arr.cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        CudaMemObj _cu_na_arr = cudaMalloc(a_arr.arr_size * CU_DOUBLE);
+        cuda_div(threadsPerBlock, a_arr.arr_size, a_arr.cu_na_arr, b_arr.cu_na_arr, _cu_na_arr);
+        cudaDeviceSynchronize();
         return new NDCuArray(_cu_na_arr, a_arr.arr_size, a_arr.shape, a_arr.stride);
     }
 
-    private int cal_idx_loc(int... indexes) {
+    int cal_idx_loc(int... indexes) {
         int index = 0;
         for (int i = 0; i < shape.length; i++) {
             index+=indexes[i] * stride[i];
